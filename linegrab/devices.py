f9cccb90073ff873be6983301838676e8fe4fc79
Nathan Harrington <nharrington@wasatchphotonics.com>
2015-09-11 15:51:55 -0400

    dalsa pipe setup

--- linegrab/devices.py
+++ linegrab/devices.py
@@ -2,7 +2,9 @@
 communication with camera control devices.
 """
 
+import sys
 import numpy
+import struct
 import logging
         
 from subprocess import Popen, PIPE
@@ -15,28 +17,94 @@ class DalsaCobraDevice(object):
     """
     def __init__(self):
         super(DalsaCobraDevice, self).__init__()
-        log.debug("Startup")
+        log.debug("DCD Startup")
 
 
     def setup_pipe(self):
+        """ Create a pipe connection to the csharp version of the single
+        line grabber on the console example from Dalsa.
+        """
         log.info("Setup pipe device")
-        prefix = "tools\GrabConsole\CSharp\\bin\Debug\\"
+        prefix = "linegrab\\GrabConsole\\CSharp\\bin\Debug\\"
 
-        cmd = 'tools\GrabConsole\CSharp\\bin\Debug\SapNETCSharpGrabConsole.exe'
-        ccf = 'tools\GrabConsole\CSharp\\bin\Debug\prcinternal.ccf'
+        cmd = "%s\\SapNETCSharpGrabConsole.exe" % prefix
+        ccf = "%s\\prcinternal.ccf" % prefix
+        print("open %s, %s" % (cmd, ccf))
         try:
             self.pipe = Popen([cmd, 
                         'grab','Xcelera-CL_LX1_1','0',
                         ccf], stdin=PIPE, stdout=PIPE)         
-            #self.pipe = Popen([cmd, 
-                        #'grab','Xcelera-CL_PX4_1','0',
-                        #ccf], stdin=PIPE, stdout=PIPE)         
         except:
-            self.log.critical("Failure to setup pipe: " + str(sys.exc_info()))
-            sys.exit(1)
-        return 1
+            log.critical("Failure to setup pipe: " + str(sys.exc_info()))
+            print("Failure to setup pipe: " + str(sys.exc_info()))
+            return False
+
         return True
 
+
+    def grab_pipe(self):
+        """ Issue a newline, get a line of data over the pipes.
+        """
+        pipe = self.pipe
+
+        line = pipe.stdout.readline().replace('\n','')
+        log.info("READ " + str(line))
+        log.info("WR enter to trigger snap")
+        log.info("\n")
+        pipe.stdin.write("\n")
+
+        line = pipe.stdout.readline().replace('\n','')
+        log.info("READ " + str(line))
+        log.info("WR enter to trigger save")
+        log.info("\n")
+        pipe.stdin.write("\n")
+
+        line = pipe.stdout.readline().replace('\n','')
+        log.info("READ " + str(line))
+        log.info("\n")
+
+        #time.sleep(1)
+        log.info("Open file")
+        result, data = self.grab_data("test.raw")
+        log.info(str(data[0:3]))
+        log.info("Done file")
+        #time.sleep(1)
+
+        log.info("WR enter to trigger repeat")
+        pipe.stdin.write("\n")
+        line = pipe.stdout.readline().replace('\n','')
+        log.info("READ " + str(line))
+        log.info("\n")
+       
+        return result, data
+
+    def grab_data(self, in_filename="tools\\test.raw"):
+        """ Read from the given raw pixel file as extracted from the
+        DALSA command line example program. Return numpy array of pixel
+        values.
+        """
+        img_data = []
+        try:
+            in_file = open(in_filename, 'rb')
+            all_data = in_file.read()
+            in_file.close()
+            pos = 0
+            while pos < 4095:
+                pixel_one = all_data[pos] + all_data[pos+1]
+                data_pak = struct.unpack("H", pixel_one)
+                img_data.append(data_pak)
+                pos += 2
+
+            return 1, img_data
+        except:
+            log.critical("Problem reading " + str(in_filename) + \
+                          str(sys.exc_info()))
+            return 0, "fail"
+
+        return 0, "done"
+
+
+    
 class SimulatedPipeDevice(object):
     """ Use the pipe device interface, return a cycling test pattern of
     data.
--- linegrab/test/test_dalsacobra.py
+++ linegrab/test/test_dalsacobra.py
@@ -2,8 +2,19 @@
 """
 
 import unittest
+import logging
+
+from testfixtures import LogCapture
 
 from linegrab.devices import DalsaCobraDevice
+
+log = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
+frmt = logging.Formatter()
+hndl = logging.StreamHandler()
+
+hndl.setFormatter(frmt)
+log.addHandler(hndl)
 
 class Test(unittest.TestCase):
 
@@ -11,10 +22,23 @@ class Test(unittest.TestCase):
         self.dev = DalsaCobraDevice()
         #self.log_capture = LogCapture()
         #self.log_name = "linegrab.devices"
-        pass
 
-    def tearDown(self):
-        pass
+    #def tearDown(self):
+        #self.log_capture.uninstall()
+
+    #def test_log_captures(self):
+        ## verification of log matching functionality
+        #from logging import getLogger
+        #getLogger().info('a message')
+        #self.log_capture.check(('root', 'INFO', 'a message'))
+
+    #def test_module_logging(self):
+        #self.assertTrue(self.dev.setup_pipe())
+#
+        #gr = self.log_name
+        #self.log_capture.check(
+            ##(gr, "INFO", "Setup pipe device"),
+            #)
 
     def test_pipe_cycle(self):
         self.assertTrue(self.dev.setup_pipe())
