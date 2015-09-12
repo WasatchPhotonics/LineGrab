1883f40852e580e6f49d0cfb4339cbb2aff7e613
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-09-12 19:39:15 -0400

    graph button ranges

--- linegrab/test/test_controller.py
+++ linegrab/test/test_controller.py
@@ -16,6 +16,13 @@ from linegrab import controller
 #    writing-unit-tests-for-qgis-python-plugins/
 app = QtGui.QApplication([])
 
+        
+# 200, 28 of the main form window location of the zoom tool
+select_pos = QtCore.QPoint(152, 28)
+zoom_pos = QtCore.QPoint(200, 28)
+extent_pos = QtCore.QPoint(257, 28)
+reset_pos = QtCore.QPoint(305, 28)
+
 class ArgsSimulation(object):
     """ Simulation of parser args in LGController.
     """
@@ -41,12 +48,9 @@ class Test(unittest.TestCase):
         QtTest.QTest.qWait(2000)
         self.assertFalse(self.form.isVisible())
 
-    def test_select_zoom_toggle(self):
+    def test_graph_buttons(self):
         args = ArgsSimulation()
         self.form.set_parameters(args)
-
-        # Wait, click the zoom icon
-        QtTest.QTest.qWait(100)
 
         # You'd think you could do: 
         # mouseClick(self.form.zoom_tool, QtCore.QtLeftButton)
@@ -60,15 +64,27 @@ class Test(unittest.TestCase):
         # shows that you should use coordinates, look up the child, then
         # the click works.
 
-        # 200, 28 of the main form window location of the zoom tool
-        zoom_pos = QtCore.QPoint(200, 28)
+        QtTest.QTest.qWait(100)
+
+        # Click zoom icon, make sure auto scale turns off
         child = self.form.childAt(zoom_pos)
-        QtTest.QTest.mouseClick(child, QtCore.Qt.LeftButton, delay=1)
-
-        # Make sure auto scale is off
-        QtTest.QTest.qWait(500)
+        QtTest.QTest.mouseClick(child, QtCore.Qt.LeftButton)
         self.assertFalse(self.form.auto_scale)
-        
 
+        # Click reset tool, make sure auto scale is on
+        child = self.form.childAt(reset_pos)
+        QtTest.QTest.mouseClick(child, QtCore.Qt.LeftButton)
+        self.assertTrue(self.form.auto_scale)
+   
+        # Click the full extent graph, verify auto scale is off and the
+        # range is 12 bit
+        child = self.form.childAt(extent_pos)
+        QtTest.QTest.mouseClick(child, QtCore.Qt.LeftButton)
+        self.assertFalse(self.form.auto_scale)
+
+        local_plot = self.form.main_curve_dialog.get_plot()
+        self.assertEqual(local_plot.get_axis_limits(0)[0], 0)
+        self.assertEqual(local_plot.get_axis_limits(0)[1], 4096)
+        
 if __name__ == "__main__":
     unittest.main()
